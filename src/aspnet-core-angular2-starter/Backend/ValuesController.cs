2037051c4dfcb86d4382ea31228168c1a78f0756
Krzysztof Koziarski <krzyhook@gmail.com>
2016-09-19 22:11:35 +0200

    Api controller moved to Backend

--- src/aspnet-core-angular2-starter/Backend/ValuesController.cs
+++ src/aspnet-core-angular2-starter/Backend/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreAngular2Starter.Controllers
+namespace AspNetCoreAngular2Starter.Backend
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
