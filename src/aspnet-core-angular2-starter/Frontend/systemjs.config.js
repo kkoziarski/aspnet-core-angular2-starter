e687ac99f151ebaa6ad6b559be60fd775ef4f075
Krzysztof Koziarski <krzyhook@gmail.com>
2016-11-22 19:42:37 +0100

    systemjs.config.js updated to be same as in angular quickstart

--- src/aspnet-core-angular2-starter/Frontend/systemjs.config.js
+++ src/aspnet-core-angular2-starter/Frontend/systemjs.config.js
@@ -2,47 +2,44 @@
  * System configuration for Angular 2 samples
  * Adjust as necessary for your application needs.
  */
-(function(global) {
-  // map tells the System loader where to look for things
-  var map = {
-    'app':                        'app', // 'dist',
-    '@angular':                   'libs/@angular',
-    'angular-in-memory-web-api': 'libs/angular-in-memory-web-api',
-    'rxjs':                       'libs/rxjs'
-  };
-  // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-  };
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade'
-  ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  var config = {
-    map: map,
-    packages: packages
-  };
-  System.config(config);
+
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'libs:': 'libs/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+
+      // angular bundles
+      '@angular/core': 'libs:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'libs:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'libs:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'libs:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'libs:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'libs:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'libs:@angular/router/bundles/router.umd.js',
+      '@angular/router/upgrade': 'libs:@angular/router/bundles/router-upgrade.umd.js',
+      '@angular/forms': 'libs:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade': 'libs:@angular/upgrade/bundles/upgrade.umd.js',
+      '@angular/upgrade/static': 'libs:@angular/upgrade/bundles/upgrade-static.umd.js',
+
+      // other libraries
+      'rxjs':                      'libs:rxjs',
+      'angular-in-memory-web-api': 'libs:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      }
+    }
+  });
 })(this);
