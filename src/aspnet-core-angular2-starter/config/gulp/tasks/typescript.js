60d64a5e6a29d0e0fbc8809694d422f49047ce89
Krzysztof Koziarski <krzyhook@gmail.com>
2016-12-04 14:09:12 +0100

    typings moved to @types

--- src/aspnet-core-angular2-starter/config/gulp/tasks/typescript.js
+++ src/aspnet-core-angular2-starter/config/gulp/tasks/typescript.js
@@ -5,18 +5,8 @@ var sourcemaps = require('gulp-sourcemaps');
 var gulpprint = require('gulp-print');
 var config = require('../config')();
 
-/*
-gulp-typescript: ts(tsProject) has been deprecated - use .pipe(tsProject(reporter)) instead
-  As of gulp-typescript 3.0, .pipe(ts(tsProject)) should be written as .pipe(tsProject()).
-  More information: http://dev.ivogabe.com/gulp-typescript-3/
-*/
-
 var tsProject = ts.createProject('Frontend/tsconfig.json');
-// /* Initialize TS Project */
-var typingFiles = [
-    'Frontend/typings/index.d.ts'
-    // config.src + 'manual_typings/**/*.d.ts'
-];
+
 var tsFiles = config.src.tsFiles; //[].concat(config.tsFiles, tsUnitFiles, tsE2EFiles);
 
 // TypeScript compile
@@ -35,7 +25,7 @@ gulp.task('watch.ts', function () {
 function compileTs(files, watchMode) {
     watchMode = watchMode || false;
 
-    var allFiles = [].concat(files, typingFiles);
+    var allFiles = files; //[].concat(files, typingFiles);
     var res = gulp.src(allFiles, {
             base: config.src.app,
             outDir: config.dest.app
@@ -45,7 +35,6 @@ function compileTs(files, watchMode) {
         // }))
         // .pipe(tslint.report())
         .pipe(sourcemaps.init())
-        //.pipe(ts(tsProject))
         .pipe(tsProject())
         .on('error', function () {
             if (watchMode) {
@@ -57,25 +46,3 @@ function compileTs(files, watchMode) {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dest.app));
 }
-
-// gulp.task('tsc-app', ['clean-ts-app', 'env'], function () {
-//     return compileTs(config.tsFiles);
-// });
-
-// // TypeScript compile
-// // ts - simple non-config - task to transpile TypeScript files to JavaScript using Gulp-TypeScript 
-// var tscConfig = require('../../../tsconfig.json');
-// gulp.task('ts', function () {
-//     return tsProject.src()
-//         .pipe(sourcemaps.init()) 
-//         .pipe(ts(tsProject))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest(config.dest.app))
-//         // .pipe(gulp.dest(tscConfig.compilerOptions.outDir))
-//         .pipe(gulpprint(function (filepath) {
-//             return util.colors.green("Compiling: " + filepath);
-//         }));
-// });
-// gulp.task('x-watch.ts', function() {
-//     return gulp.watch(tsFiles, ['ts']);
-// });
